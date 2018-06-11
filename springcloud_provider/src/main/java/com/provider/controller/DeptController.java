ffc9dd595623939c0f7721f58b794ba9873cb773
caoyang <aries_caoyang@163.com>
2018-06-11 22:37:38 +0800

    Eureka集群

--- springcloud_provider/src/main/java/com/provider/controller/DeptController.java
+++ springcloud_provider/src/main/java/com/provider/controller/DeptController.java
@@ -3,12 +3,17 @@ package com.provider.controller;
 import com.provider.service.DeptService;
 import com.springcloud.bean.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class DeptController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     private DeptService service;
@@ -26,5 +31,16 @@ public class DeptController {
     @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
     public List<Dept> list(){
         return service.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery",method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        System.out.println("************"+list);
+        List<ServiceInstance> serList = discoveryClient.getInstances("microservicecloud-cy-dept");
+        for (ServiceInstance element:serList){
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
