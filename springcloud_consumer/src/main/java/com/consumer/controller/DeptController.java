a921c6f9fcd7166d6f4e0d3fb9f5ee8992947d57
caoyang <aries_caoyang@163.com>
2018-06-11 23:38:02 +0800

    Ribbon初步配置

--- springcloud_consumer/src/main/java/com/consumer/controller/DeptController.java
+++ springcloud_consumer/src/main/java/com/consumer/controller/DeptController.java
@@ -12,7 +12,8 @@ import java.util.List;
 @RestController
 public class DeptController {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+//    private static final String REST_URL_PREFIX = "http://localhost:8001";
+      private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-CY-DEPT";
     /**
      * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
      * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
