25fe9442f5194346d337350cd8b1e661413f4d2b
caoyang <aries_caoyang@163.com>
2018-06-12 23:12:08 +0800

    Ribbon负载均衡

--- springcloud_consumer/src/main/java/com/consumer/cfgbeans/ConfigBean.java
+++ springcloud_consumer/src/main/java/com/consumer/cfgbeans/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.consumer.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,10 @@ public class ConfigBean {
     @LoadBalanced       //spring cloud ribbon 是基于Netflix Ribbon实现的一套客户端的负载均衡
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();    //达到目的，用我们重新选择的随机算法替代默认的轮询
     }
 }
